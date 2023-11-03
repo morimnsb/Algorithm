5ae741dde3aa5032f52211630c5ff425abbf950b
Mori <126830209+morimnsb@users.noreply.github.com>
2023-11-03 13:41:53 +0100

    Update Friday-algorithm.js

--- Friday-algorithm.js
+++ Friday-algorithm.js
@@ -5,12 +5,26 @@
 
 // Using the array [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31], the correct answer is : 11
 
+//Solution 1 with sort()
 
 // Have an array 
 // sort from big to small 
 // call the third Element
 
 
+// var array = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31]
+// array.sort(function(a,b){return b - a;});
+// console.log(array[2]);
+
+
+
+// Solution 2 Without sort()
+
+// have an array 
+// remove two first biggest value from araay
+// log the Biggest value
+
 var array = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31]
-array.sort(function(a,b){return b - a;});
-console.log(array[2]);
+for (var i = 0; i < 2; i++){
+  array.splice(array.indexOf(Math.max.apply(null,array)),1 )}
+console.log(Math.max.apply(null,array));
