8f67b97624f9954d91f5ebac314524d3d469c23d
Mori <126830209+morimnsb@users.noreply.github.com>
2023-11-15 15:58:12 +0100

    Add files via upload

---
+++ sorting-algorithm/bubbleSort.js
@@ -0,0 +1,17 @@
+//Pseudocode
+// make a function that takes an Array as input
+// in a while loop  ,Take each and compare pair of 2 adjacent numbers and destruct them in small order
+// return Array
+
+let arr= [1,5,3,77,54,7,-9,-57,23,65,41,12,2,0]
+
+function bubbleSort(arr){
+  for (let i = 0; i < arr.length; i++) {
+     while (arr[i] > arr[i+1]) {
+      [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
+      i--
+     }
+  }
+  return arr
+}
+console.log(bubbleSort(arr));
