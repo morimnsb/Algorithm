8f47331a23f9a46d95b27dd0f66003860cdd0dd7
Mori <126830209+morimnsb@users.noreply.github.com>
2023-11-15 16:21:47 +0100

    selection-sort.js

--- sorting-algorithm/selection-sort.js
+++ sorting-algorithm/selection-sort.js
@@ -12,9 +12,7 @@ function selectionSort(arr){
   for (let i=0; i<arr.length; i++) {
     for (let j = i ; j < arr.length; j++) {
     if (arr[i] > arr[j] ) {
-      temp = arr[i]
-      arr[i] = arr[j]
-      arr[j] = temp
+      [arr[j], arr[i]] = [arr[i], arr[j]];
     }}}
   return arr
 }
