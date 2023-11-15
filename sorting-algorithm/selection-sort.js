c941b661ed7268017404048d4b9bc23889b9fbc8
Mori <126830209+morimnsb@users.noreply.github.com>
2023-11-15 13:38:53 +0100

    Add files via upload

---
+++ sorting-algorithm/selection-sort.js
@@ -0,0 +1,21 @@
+// Tuesday : Selection Sort
+
+//Pseudocode
+
+// make a function that takes a array 
+// loop the array and compare each index with the rest by another for loop  
+// check and replace smaller number by the bigger number
+// return Array
+
+let arr= [1,5,3,77,54,7,-9,-57,23,65,41,12,2,0]
+function selectionSort(arr){
+  for (let i=0; i<arr.length; i++) {
+    for (let j = i ; j < arr.length; j++) {
+    if (arr[i] > arr[j] ) {
+      temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
+    }}}
+  return arr
+}
+console.log(selectionSort(arr));
